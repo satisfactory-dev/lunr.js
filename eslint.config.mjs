c6fab0ed1d9fb93f92cb0eccc627bfa80457ff08
SignpostMarv <me@signpostmarv.name>
2026-01-12 13:06:30 +0000

    switch to import.meta.dirname

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,15 +4,11 @@ import {
 } from "eslint/config"
 import spellcheck from "eslint-plugin-spellcheck"
 import globals from "globals"
-import path from "node:path"
-import { fileURLToPath } from "node:url"
 import js from "@eslint/js"
 import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all
 })
